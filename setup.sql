71fdaab33591e59feb1eb3d81adbd03de3fa8f50
Yun Heng <yun@yun.sg>
2016-04-10 19:27:14 +0800

    update sql file

--- setup.sql
+++ setup.sql
@@ -1,14 +1,15 @@
-DROP TABLE IF EXISTS `access`; 
+DROP TABLE IF EXISTS `access`;
 CREATE TABLE `access` (
   `userId` varchar(64) NOT NULL,
   `lockId` varchar(16) NOT NULL,
   PRIMARY KEY (`userId`,`lockId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `access_tokens`; 
+DROP TABLE IF EXISTS `access_tokens`;
 CREATE TABLE `access_tokens` (
   `userId` varchar(64) NOT NULL,
-  `token` varchar(32) NOT NULL,
+  `token` varchar(128) NOT NULL,
+  `secretkey` BLOB,
   `issued` TIMESTAMP NOT NULL,
   `expiry` TIMESTAMP NOT NULL,
   PRIMARY KEY (`userId`, `issued`, `expiry`)
@@ -24,14 +25,14 @@ CREATE TABLE `otp_tokens` (
   PRIMARY KEY (`userId`)
 );
 
-DROP TABLE IF EXISTS `locks`; 
+DROP TABLE IF EXISTS `locks`;
 CREATE TABLE `locks` (
   `id` varchar(16) NOT NULL,
   `description` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `users`; 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` varchar(64) NOT NULL,
   `email` varchar(256) NOT NULL,
@@ -44,9 +45,9 @@ CREATE TABLE `users` (
 ALTER TABLE `access`
   ADD CONSTRAINT `access_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `access_ibfk_2` FOREIGN KEY (`lockId`) REFERENCES `locks` (`id`);
-  
+
 ALTER TABLE `otp_tokens`
   ADD CONSTRAINT `otp_tokens_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
-  
+
 ALTER TABLE `access_tokens`
   ADD CONSTRAINT `access_tokens_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
